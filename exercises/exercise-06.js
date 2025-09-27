024e3c2a8ad83ccfb4c78dca3b2edb23b4149dd0
Anthony Caliani <avcaliani.it@gmail.com>
2025-09-27 16:39:49 -0300

    feat: Solution for exercise 06

--- exercises/exercise-06.js
+++ exercises/exercise-06.js
@@ -1,29 +1,41 @@
 // -----------------------
-//  exercise-06.js
+//  exercise-01.js
 // ------------------
-// In this file you will find a suggested structure for your code.
-// You can modify it as you want, as long as you reach the expected result. 
 
 async function getAllSpells() {
-    // Add your code here...
-    // If you need to add parameters in the function signature, 
-    // feel free to do so.
-    return [];
+    const response = await fetch("https://wizard-world-api.herokuapp.com/Spells");
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const spells = await response.json();
+    return spells;
 }
 
 function filterSpells(spells) {
     const filteredSpells = [];
-    // Add your code here...
+    for (const spell of spells) {
+        const numberOfAs = spell.name.toLowerCase().split("a").length - 1;
+        if (numberOfAs > 1) {
+            filteredSpells.push(spell);
+        }
+    }
     return filteredSpells;
 }
 
 function printSpells(spells) {
-    // Add your code here...
+    console.log("-+-+-+-+-+-+-+-+-+-");
+    console.log(" List of Spells ✨");
+    console.log("-+-+-+-+-+-+-+-+-+-");
+    for (const spell of spells) {
+        console.log(` → ${spell.name}`);
+    }
 }
 
 // -----------------------
 // Running the code 🚀
 // -----------------------
 const allSpells = await getAllSpells();
+console.log(`There are ${allSpells.length} spells in total.`);
 const filteredSpells = filterSpells(allSpells);
+console.log(`Only ${filteredSpells.length} spells have two or more letter 'A' in the name.`);
 printSpells(filteredSpells);
