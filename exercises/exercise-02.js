0fff31965874734dbab16e07fc5b17baa29cfd8b
Anthony Caliani <avcaliani.it@gmail.com>
2025-09-27 16:16:49 -0300

    feat: Solution for exercise 02

--- exercises/exercise-02.js
+++ exercises/exercise-02.js
@@ -5,14 +5,18 @@
 // You can modify it as you want, as long as you reach the expected result. 
 
 async function getSpell(spellId) {
-    // Add your code here...
-    // If you need to add parameters in the function signature, 
-    // feel free to do so.
-    return {};
+    const response = await fetch(`http://localhost:3000/spells/${spellId}`);
+    if (!response.ok) {
+        console.error(`‼️ HTTP error! Status: ${response.status} | Spell ${spellId} not found!`);
+        return null;
+    }
+    const spell = await response.json();
+    return spell;
 }
 
-function printSpellEffect(spells) {
-    // Add your code here...
+function printSpellEffect(spell) {
+    if (!spell) return;
+    console.log(`✨ The spell "${spell.name}" effect is to ${spell.effect.toLowerCase()}.`);
 }
 
 // -----------------------
