447dcabccfcd18603a3e93c0ad7db9b87507c4a5
Isabelle VC <isabellecaliani@gmail.com>
2025-09-27 17:49:39 -0300

    Exercise 1 answer

--- exercises/exercise-01.js
+++ exercises/exercise-01.js
@@ -5,25 +5,38 @@
 // You can modify it as you want, as long as you reach the expected result. 
 
 async function getAllSpells() {
-    // Add your code here...
-    // If you need to add parameters in the function signature, 
-    // feel free to do so.
-    return [];
+    const response = await fetch("http://localhost:3000/spells");
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const spells = await response.json();
+    return spells;
 }
 
 function filterSpells(spells) {
     const filteredSpells = [];
+    
+    for (var i=0; i <= spells.length - 1; i++){
+        const spellName = spells[i].name.toLowerCase()
+        if (spellName.split("a").length >= 3){
+            
+            filteredSpells.push(spellName)
+        }
+    }
     // Add your code here...
     return filteredSpells;
 }
 
-function printSpells(spells) {
-    // Add your code here...
-}
+// function printSpells(spells) {
+//     // Add your code here...
+// }
 
 // -----------------------
 // Running the code 🚀
 // -----------------------
 const allSpells = await getAllSpells();
+console.log(`Spells Found: ${allSpells.length}`)
+console.log(allSpells)
+
 const filteredSpells = filterSpells(allSpells);
-printSpells(filteredSpells);
+console.log(filteredSpells);
