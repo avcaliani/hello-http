0651bae78503b4f2dbe6ad757905244acc1df66e
Anthony Caliani <avcaliani.it@gmail.com>
2025-09-27 16:04:51 -0300

    feat: Exercise 02 template.

---
+++ exercises/exercise-02.js
@@ -0,0 +1,25 @@
+// -----------------------
+//  exercise-02.js
+// ------------------
+// In this file you will find a suggested structure for your code.
+// You can modify it as you want, as long as you reach the expected result. 
+
+async function getSpell(spellId) {
+    // Add your code here...
+    // If you need to add parameters in the function signature, 
+    // feel free to do so.
+    return {};
+}
+
+function printSpellEffect(spells) {
+    // Add your code here...
+}
+
+// -----------------------
+// Running the code 🚀
+// -----------------------
+const spellFive = await getSpell("5");
+printSpellEffect(spellFive);
+
+const spellSeventeen = await getSpell("17");
+printSpellEffect(spellSeventeen);
