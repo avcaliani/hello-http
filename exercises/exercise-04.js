5d95d6244add78418c1b71b05ae8dec3d4949f27
Isabelle VC <isabellecaliani@gmail.com>
2025-11-01 17:27:31 -0300

    Updating an spell with PATCH successful

--- exercises/exercise-04.js
+++ exercises/exercise-04.js
@@ -4,13 +4,31 @@
 // In this file you will find a suggested structure for your code.
 // You can modify it as you want, as long as you reach the expected result. 
 
-async function updateSpellName(spellId, newName) {
-    // Add your code here...
-    // If you need to add parameters in the function signature, 
-    // feel free to do so.
+// Atualizar parâmetro específico do spell
+async function updateSpellName(spellId, updateParameter, newValue) {
+    const response = await fetch(`http://localhost:3000/spells/${spellId}`,{
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({ [updateParameter]: newValue}),
+    }
+    );
+    if (!response.ok) {
+        throw new Error(`‼️ HTTP error! Status: ${response.status} | Spell ${spellId} not updated!`);
+    }
+    const spellUpdate = await response.json();
+    console.log(spellUpdate)
+    return spellUpdate;
+
 }
+
+}
+
+
 
 // -----------------------
 // Running the code 🚀
 // -----------------------
-await updateSpellName("1", "Accio");
+await updateSpellName("1", "name", "Accio");
+
