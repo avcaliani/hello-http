71abda62bd96d237e875e646ba23069a8e39aa91
Isabelle VC <isabellecaliani@gmail.com>
2025-11-01 17:28:33 -0300

    Checking all spells on data base

--- exercises/exercise-05.js
+++ exercises/exercise-05.js
@@ -4,6 +4,18 @@
 // In this file you will find a suggested structure for your code.
 // You can modify it as you want, as long as you reach the expected result. 
 
+
+// Checar lista completa de spells 
+async function getAllSpells() {
+    const response = await fetch("http://localhost:3000/spells");
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const spells = await response.json();
+    return spells;
+}
+
+// Deletar um spell por ID
 async function deleteSpell(spellId) {
     // Add your code here...
     // If you need to add parameters in the function signature, 
@@ -13,5 +25,11 @@ async function deleteSpell(spellId) {
 // -----------------------
 // Running the code 🚀
 // -----------------------
+
+const allSpells = await getAllSpells()
+// Checar lista de spells ANTES do delete
+console.log(allSpells)
+
 await deleteSpell("1000");
 await deleteSpell("1001");
+
